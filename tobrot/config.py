7fabedec955f064048ca7239c805d2d512bc5818
mrmythpat <75902632+mrmythpat@users.noreply.github.com>
2020-12-13 00:39:09 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,5 +1,5 @@
 class config:
-    BOT_TOKEN = "1488122950:AAEgHAwAA-NbXgeWIVy9AheBEmTdpde-_Bo"
+    BOT_TOKEN = "1454085962:AAFkhNB5WtkH3lY-5SWCzaFdMTqVE6wz0QU"
     APP_ID = "2206386"
     API_HASH = "cbb279159c065dbb3ab1e3160b004518"
     DATABASE_URL = "postgres://dvchwqfkuuenpa:a892026422fbb220e3e64d31ab0b9a1807c483a00826d5d692b10e52b9b55c7f@ec2-18-233-207-22.compute-1.amazonaws.com:5432/da6dt8b7ukqei5"
