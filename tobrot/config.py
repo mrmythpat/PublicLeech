d5e97c8c6b669d1e3be23d0cbfe2d1d26c1de40a
mrmythpat <75902632+mrmythpat@users.noreply.github.com>
2020-12-13 17:31:44 +0530

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -2,7 +2,5 @@ class config:
     BOT_TOKEN = "1454085962:AAFkhNB5WtkH3lY-5SWCzaFdMTqVE6wz0QU"
     APP_ID = "2206386"
     API_HASH = "cbb279159c065dbb3ab1e3160b004518"
-    DATABASE_URL = "postgres://dvchwqfkuuenpa:a892026422fbb220e3e64d31ab0b9a1807c483a00826d5d692b10e52b9b55c7f@ec2-18-233-207-22.compute-1.amazonaws.com:5432/da6dt8b7ukqei5"
     SUDO_USERS = "1366454553" # Sepearted by space.
-    SUPPORT_CHAT_LINK = "@chatsupport99"
-    DOWNLOAD_DIRECTORY = "./downloads/"
+    AUTH_CHANNEL = -1001259570786
